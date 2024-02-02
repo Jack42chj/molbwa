018f6304ede91d56c15f8dce09e1b3c76c426a22
hojinchoi99 <hojinch99@gmail.com>
2024-02-02 17:37:17 +0900

    Molbwa: vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App.tsx';
 import '../src/assets/fonts/font.css';
 import "../src/styles/styles.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 );
